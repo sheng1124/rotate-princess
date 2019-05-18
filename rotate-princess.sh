ae2455cedbfb9c58c7b4153014c2cd8c4a79d5f3
sheng1124 <rhythm7321@gmail.com>
2019-05-18 17:07:32 +0800

    replace old methed, build a file trace the rotate log, rotatation rely on this file, it can work well in any system capable execute xrandr -o .

--- rotate-princess.sh
+++ rotate-princess.sh
@@ -1,17 +1,24 @@
-screen=$(xrandr)
+#!/bin/bash
 
-screenDirection=$(echo ${screen} | cut -c107-111)
-xrandr -o 1
-echo "current screen direction: ${screenDirection}"
+test -f ~/.rotateprincess/rotateVector.txt && isExist=1 || isExist=0
 
-if [ ${screenDirection} = "left" ]; then
-	echo "xrandr turn to normal"
-	xrandr -o 0
-elif [ ${screenDirection} = "(norm" ]; then
-	echo "xrandr turn to left"
-	xrandr -o 1
+if [ ${isExist} = "0" ]; then
+	mkdir ~/.rotateprincess
+	touch ~/.rotateprincess/rotateVector.txt
+	echo "normal" > ~/.rotateprincess/rotateVector.txt
+	touch ~/.rotateprincess/message
+	xrandr -o normal
+	echo "initialize complete, please excute this program again." > ~/.rotateprincess/message	
+	gedit ~/.rotateprincess/message
+	rm ~/.rotateprincess/message
 else
-	echo "the current screen resolution is not match or string cut format is wrong."
+	vector=$(head ~/.rotateprincess/rotateVector.txt)
+	if [ ${vector} = "normal" ]; then
+		xrandr -o left
+		echo "left" > ~/.rotateprincess/rotateVector.txt
+	else
+		xrandr -o normal
+		echo "normal" > ~/.rotateprincess/rotateVector.txt
+	fi
 fi
-
 exit 0
